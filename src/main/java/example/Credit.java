b61a4975617d7f80e42f5942d08a1dbb4e4d2b78
VovaBy-web <vova_by@mail.ru>
2024-09-06 16:43:14 +0300

    My commit_293

---
+++ src/main/java/example/Credit.java
@@ -0,0 +1,10 @@
+package example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Credit {
+    public static void main(String[] args) {
+        SpringApplication.run(Credit.class, args);
+    }
+}
---
+++ src/main/java/example/controllers/CreeditController.java
@@ -0,0 +1,13 @@
+package example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CreeditController {
+    @GetMapping("/")
+    public String getcredit(){
+       return "main";
+    }
+
+}
